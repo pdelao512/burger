d5dd109c98409f077d081cccc7a24ac21304a6a1
pdelao512 <pdelao512@gmail.com>
2017-08-08 18:52:18 -0500

    db

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burgers(burger_name, devoured, date)
+VALUES ("Good Seed Veggie Burger", false, "2017-08-08 04:20:00"),
+  ("Bueno Burger", false, "2017-08-08 04:20:01"),
+  ("La Bandita", false, "2017-08-08 04:20:02"),
+  ("BBQ Burger", false, "2017-08-08 16:20:00"),
+  ("Chipotle Chik'n", false, "2017-08-08 16:20:01"),
+  ("Blacksmith Burger", false, "2017-08-08 04:20:20"),
+  ("Bac'n Cheeze Burger", true, "2017-08-08 16:20:16");

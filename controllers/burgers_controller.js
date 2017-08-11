ba9e8dbd760b5ce5f34c77570a0a79240b956f6a
pdelao512 <pdelao512@gmail.com>
2017-08-11 01:08:54 -0500

    js

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,6 +1,4 @@
 // Node Dependencies
-// require in express, and set up routing for it, and bring in
-// the burger.js model file
 var express = require('express');
 var router = express.Router();
 var burger = require('../models/burger.js');
@@ -10,9 +8,7 @@ router.get('/', function(req, res) {
   res.redirect('/index');
 });
 
-// add a '/index/' endpoint that gets all the burgers
-// then renders the index file by passing in all the burgers
-// as an object for handlebars to use
+// add a '/index/' endpoint that gets all the burgers and renders the index file by passing in all the burgers as an object for handlebars to use
 router.get('/index', function(req, res) {
   burger.selectAll(function(data) {
     var hbsObject = {burgers: data};
@@ -21,18 +17,14 @@ router.get('/index', function(req, res) {
   });
 });
 
-// add a '/burgers/insertOne' endpoint that posts the 
-// burger name the user entered then as a callback it
-// redirects back to the /index route
+// add a '/burgers/insertOne' endpoint that posts the burger name the user entered then as a callback it redirects back to the /index route
 router.post('/burgers/insertOne', function(req, res) {
   burger.insertOne(['burger_name', 'devoured'], [req.body.name, false], function() {
     res.redirect('/index');
   });
 });
 
-// add a '/burgers/updateOne/:id' route that updates
-// the status of the burger from being uneaten to eaten
-// then does a callback that redirects to the /index endpoint
+// add a '/burgers/updateOne/:id' route that updates the status of the burger from being uneaten to eaten then redirects to the /index endpoint
 router.put('/burgers/updateOne/:id', function(req, res) {
   var condition = 'id = ' + req.params.id;
   console.log('condition', condition);
